8933bfced797c74520fba35820eea75d80e8c3e4
lucasma2009_00 <lucas.marques@eurekalabs.com.br>
2023-03-28 21:31:18 -0300

    Authorized request

--- frontend/src/util/requests.ts
+++ frontend/src/util/requests.ts
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import axios, { AxiosRequestConfig } from 'axios';
 import qs from 'qs';
 
 type LoginResponse = {
@@ -42,6 +42,18 @@ export const requestBackendLogin = (loginData: LoginData) => {
         headers
     })
 }
+
+export const requestBackend = (config: AxiosRequestConfig) => {
+
+    const headers = config.withCredentials 
+        ? {
+            ...config.headers,
+            Authorization: 'Bearer ' + getAuthData().access_token,
+          } 
+        : config.headers;
+
+    return axios({ ...config, baseURL: BASE_URL, headers });
+};
 
 export const saveAuthData = (obj : LoginResponse) => {
     localStorage.setItem(tokenKey, JSON.stringify(obj));

6a83b2e0350dc423f3fcc1429559cfccf9a1ec35
lucasma2009_00 <lucas.marques@eurekalabs.com.br>
2023-04-10 21:37:43 -0300

    hasAnyRoles

--- frontend/src/util/requests.ts
+++ frontend/src/util/requests.ts
@@ -3,7 +3,7 @@ import qs from 'qs';
 import history from './history';
 import jwtDecode from 'jwt-decode';
 
-type Role = 'ROLE_OPERATOR' | 'ROLE_ADMIN'
+type Role = 'ROLE_OPERATOR' | 'ROLE_ADMIN';
 
 export type TokenData = {
     exp: number,
@@ -106,4 +106,25 @@ export const isAuthenticated = () : boolean => {
     const tokenData = getTokenData();
 
     return (tokenData && tokenData.exp * 1000 > Date.now()) ? true : false;
+}
+
+export const hasAnyRoles = (roles: Role[]) : boolean => {
+    if (roles.length === 0) {
+        return true;
+    }
+    
+    const tokenData = getTokenData();
+
+    // if (tokenData !== undefined) {
+    //     return roles.some(role => tokenData.authorities.includes(role));
+    // }
+
+    if (tokenData !== undefined) {
+        for (var i=0; i< roles.length; i++) {
+            if (tokenData.authorities.includes(roles[i])) {
+                return true;
+            }
+        }
+    }
+    return false;
 }

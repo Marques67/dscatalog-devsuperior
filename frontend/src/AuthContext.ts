c4bcb9ec1b8b928b9c8bd50ebd8e84f2e569d0d0
Lucas Marques <lucasmarquesff@hotmail.com>
2024-01-31 20:35:55 -0300

    Refactor token, role

--- frontend/src/AuthContext.ts
+++ frontend/src/AuthContext.ts
@@ -1,10 +1,10 @@
 import { createContext } from "react";
-import { TokenData } from "util/auth";
+import { TokenData } from "util/token";
 
 export type AuthContextData = {
     authenticated: boolean;
     tokenData?: TokenData;
-  };
+};
 
 export type AuthContextType = {
     authContextData: AuthContextData;
